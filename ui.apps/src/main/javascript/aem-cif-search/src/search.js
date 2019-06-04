29cbec41f609673568eac8c69a2850e3ca4a9c6e
Daniel Platon <platon@adobe.com>
2019-06-04 16:16:04 +0200

    CIF-857 - Check Feasibility of Reusing Peregrin Search Component
    Add copyright headers where needed. Update the RAT plugin to ignore some files.

--- ui.apps/src/main/javascript/aem-cif-search/src/search.js
+++ ui.apps/src/main/javascript/aem-cif-search/src/search.js
@@ -1,5 +1,18 @@
+/*******************************************************************************
+ *
+ *      Copyright 2019 Adobe. All rights reserved.
+ *      This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License. You may obtain a copy
+ *      of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software distributed under
+ *      the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *      OF ANY KIND, either express or implied. See the License for the specific language
+ *      governing permissions and limitations under the License.
+ *
+ *
+ ******************************************************************************/
 import React, {useState, useEffect} from 'react';
-import {Query} from 'react-apollo';
 import PRODUCT_SEARCH from './queries/ProductSearch.graphql';
 
 import {useQuery} from '@magento/peregrine';
